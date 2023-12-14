5031dd29f231cff218f4dc57cc6128beba06e52c
Omar-Arja <omaralarja17@gmail.com>
2023-12-14 10:16:42 +0200

    Refactored user table component, integrated Ngrx store for state management & removed hardcoded data

--- src/app/components/user-table/user-table.component.ts
+++ src/app/components/user-table/user-table.component.ts
@@ -1,100 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { TableModule } from 'primeng/table';
+import { AsyncPipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
+import { Observable } from 'rxjs';
 import { User } from '../../models/user.model';
+import { Store } from '@ngrx/store';
+import { UserState } from '../../store/reducers/user.reducer';
+import { getUsers } from '../../store/selectors/user.selectors';
+import * as UserActions from '../../store/actions/user.actions';
 
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
   styleUrls: ['./user-table.component.css'],
   standalone: true,
-  imports: [TableModule],
+  imports: [TableModule, AsyncPipe, DatePipe],
 })
 export class UserTableComponent implements OnInit {
-
+  users$: Observable<User[]> = this.store.select(getUsers);
   users: User[] = [];
 
+  constructor(private store: Store<UserState>) {}
+
   ngOnInit(): void {
-    this.users = [
-      {
-        id: 1,
-        username: 'johndoe',
-        fullName: 'John Doe',
-        email: 'john.doe@example.com',
-        age: 35,
-        isActive: true,
+    this.store.dispatch(UserActions.loadUsers());
+    this.users$.subscribe(
+      (users) => {
+        const usersArray = (users as unknown as { users: User[] }).users;
+        this.users = usersArray;
       },
-      {
-        id: 2,
-        username: 'janedoe',
-        fullName: 'Jane Doe',
-        email: 'jane.doe@example.com',
-        age: 30,
-        isActive: false,
-      },
-      {
-        id: 3,
-        username: 'michelangelo',
-        fullName: 'Michelangelo Buonarroti',
-        email: 'michelangelo@example.com',
-        age: 58,
-        isActive: false,
-      },
-      {
-        id: 4,
-        username: 'leonardodavinci',
-        fullName: 'Leonardo da Vinci',
-        email: 'leonardo@example.com',
-        age: 67,
-        isActive: true,
-      },
-      {
-        id: 5,
-        username: 'marie_curie',
-        fullName: 'Marie Curie',
-        email: 'marie@example.com',
-        age: 66,
-        isActive: true,
-      },
-      {
-        id: 6,
-        username: 'albert_einstein',
-        fullName: 'Albert Einstein',
-        email: 'albert@example.com',
-        age: 76,
-        isActive: true,
-      },
-      {
-        id: 7,
-        username: 'stephenhawking',
-        fullName: 'Stephen Hawking',
-        email: 'stephen@example.com',
-        age: 76,
-        isActive: false,
-      },
-      {
-        id: 8,
-        username: 'ada_lovelace',
-        fullName: 'Ada Lovelace',
-        email: 'ada@example.com',
-        age: 36,
-        isActive: false,
-      },
-      {
-        id: 9,
-        username: 'marie_anne_pierrette_paulze',
-        fullName: 'Marie-Anne Pierrette Paulze (Madame Curie)',
-        email: 'marieanne@example.com',
-        age: 66,
-        isActive: true,
-      },
-      {
-        id: 10,
-        username: 'nelson_mandela',
-        fullName: 'Nelson Mandela',
-        email: 'nelson@example.com',
-        age: 95,
-        isActive: false,
-      },
-    ];
+    );
   }
 }

c1f28155d4760ab99318cac4813b1f619e51372f
Omar-Arja <omaralarja17@gmail.com>
2023-12-14 10:12:33 +0200

    Created user actions for CRUD operations

---
+++ src/app/store/actions/user.actions.ts
@@ -0,0 +1,21 @@
+import { createAction, props } from '@ngrx/store';
+import { User } from '../../models/user.model';
+
+export const loadUsers = createAction('[User] Load Users');
+
+export const loadUsersSuccess = createAction(
+  '[User] Load Users Success',
+  props<{ users: User[] }>()
+);
+
+export const addUser = createAction('[User] Add User', props<{ user: User }>());
+
+export const updateUser = createAction(
+  '[User] Update User',
+  props<{ user: User }>()
+);
+
+export const deleteUser = createAction(
+  '[User] Delete User',
+  props<{ userId: number }>()
+);

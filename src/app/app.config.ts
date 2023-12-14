e0f97aa902a8c7355fb7c4aaca3577f5d785f279
Omar-Arja <omaralarja17@gmail.com>
2023-12-14 10:24:47 +0200

    Configured application providers for effects, store, HTTP client, and DevTools integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideEffects } from '@ngrx/effects';
+import { UserEffects } from './store/effects/user.effects';
+import { userReducer } from './store/reducers/user.reducer';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideEffects([UserEffects]),
+    provideStore({ users: userReducer }),
+    provideHttpClient(),
+    provideStoreDevtools(),
+  ],
 };
